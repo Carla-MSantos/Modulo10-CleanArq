a994cc7a466ae4f6dc42a6f0bb3178cd72f7ba95
Carla-MSantos <163577215+Carla-MSantos@users.noreply.github.com>
2025-01-29 16:32:25 -0300

    Desafio API
    ajuste no desafio API

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -10,34 +10,34 @@ describe("E2E test for product", () => {
     await sequelize.close();
   });
 
-  it("should create a product", async () => {
+  it("it should create a product", async () => {
     const response = await request(app).post("/product").send({
-      name: "Product A",
+      name: "Product 1",
       price: 10,
     });
 
     expect(response.status).toBe(200);
-    expect(response.body.name).toBe("Product A");
+    expect(response.body.name).toBe("Product 1");
     expect(response.body.price).toBe(10);
   });
 
-  it("should not create a product", async () => {
+  it("it should not create a product", async () => {
     const response = await request(app).post("/product").send({
-      name: "Product A",
+      name: "Product 1",
       price: -1,
     });
     expect(response.status).toBe(500);
   });
 
-  it("should list all product", async () => {
+  it("it should list all product", async () => {
     const response = await request(app).post("/product").send({
-      name: "Product A",
+      name: "Product 1",
       price: 10,
     });
     expect(response.status).toBe(200);
 
     const response2 = await request(app).post("/product").send({
-      name: "Product B",
+      name: "Product 2",
       price: 20,
     });
     expect(response2.status).toBe(200);
@@ -47,10 +47,10 @@ describe("E2E test for product", () => {
     expect(listResponse.status).toBe(200);
     expect(listResponse.body.products.length).toBe(2);
     const product = listResponse.body.products[0];
-    expect(product.name).toBe("Product A");
+    expect(product.name).toBe("Product 1");
     expect(product.price).toBe(10);
     const product2 = listResponse.body.products[1];
-    expect(product2.name).toBe("Product B");
+    expect(product2.name).toBe("Product 2");
     expect(product2.price).toBe(20);
   });
 });
